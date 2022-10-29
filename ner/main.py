b49140a471ee67a4815b7e5e29898d4ed893bd4b
Benjamin Labrecque <labrecque.benji@gmail.com>
2022-10-29 17:19:00 +0200

    Reformat code

--- ner/main.py
+++ ner/main.py
@@ -1,13 +1,34 @@
-from transformers import AutoTokenizer, AutoModelForTokenClassification
-from transformers import pipeline
+from pydantic import BaseModel
+from transformers import AutoModelForTokenClassification, AutoTokenizer, pipeline
+from fastapi import FastAPI
+
+MODEL = "dslim/bert-base-NER"
+PIPELINE = "ner"
+
+EXAMPLE = "My name is Wolfgang and I live in Berlin."
+"I like Apple and my favourite fruit is the Apple."
+
+fast_api = FastAPI()
+
+
+class Entity(BaseModel):
+    word: str
+    entity: str
+    start: int
+    end: int
+    index: int
+    score: float
+
+
+def get_model():
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    model = AutoModelForTokenClassification.from_pretrained(MODEL)
+    return model
+
 
 if __name__ == "__main__":
+    model = get_model()
+    nlp = pipeline(PIPELINE, model=model, tokenizer=tokenizer)
 
-    tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
-    model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
-
-    nlp = pipeline("ner", model=model, tokenizer=tokenizer)
-    example = "My name is Wolfgang and I live in Berlin. I like Apple and my favourite fruit is the Apple."
-
-    ner_results = nlp(example)
+    ner_results = [Entity(**e) for e in nlp(EXAMPLE)]
     print(ner_results)
